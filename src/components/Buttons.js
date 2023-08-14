f1d756dab7a1219d8aaaafa4075c137d4ecdfafe
rumneel <139188968+rumneel@users.noreply.github.com>
2023-08-14 12:20:06 -0500

    Update Buttons.js

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,9 +1,15 @@
-const Buttons = () => {
-  return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
-      </div>
+import satData from './satData.js'; 
+import './styling.css';
+
+function Buttons({filterByType,setSat,displaySats}) {
+    return (
+    <div className ="flex-container">
+      {displaySats.map((sat, id) => (
+        <button onClick={() => filterByType(sat)} key={id}>
+          {sat} Orbit </button>
+      ))}
+      <button onClick={() => setSat(satData)}>All Orbits</button>
+    </div>
   );
 };
 
